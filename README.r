0e1905ad01742614a1245b06233d4691ddf444dd
Laura Marshall <L_H_Marshall@yahoo.co.uk>
2015-11-17 20:58:41 +0000

    Update README.r

--- README.r
+++ README.r
@@ -1,3 +1,11 @@
-setwd("C:/Users/Laura/mads")
-roxygenize(package.dir = "mads")
+`mads` - Multi-Analysis Distance Sampling
+-----------------------------------------
+
+[![CRAN (RStudio Mirror) Downloads](http://cranlogs.r-pkg.org/badges/mads)](http://www.r-pkg.org/pkg/mads)
+[![CRAN Version](http://www.r-pkg.org/badges/version/mads)](http://www.r-pkg.org/pkg/mads)
+
+# What is `dsm`?
+
+`mads` provides a users with the option of incorporating unidentified sightings, covariate uncertainty and model uncertainty into distance sampling analyses.
+
 
